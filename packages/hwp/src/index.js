bbf2f0a3ee36040bddac9040c8b60780ed6d64b3
kohm0880-svg <kohm0880@gmail.com>
2026-09-11 06:43:50 +0900

    refactor(hwp): expose native parser only

--- packages/hwp/src/index.js
+++ packages/hwp/src/index.js
@@ -1,1 +1,6 @@
-export * from './importer.js';
+export * from './cfb-reader.js';
+export * from './file-header.js';
+export * from './record-reader.js';
+export * from './tags.js';
+export * from './body-parser.js';
+export * from './native-parser.js';
